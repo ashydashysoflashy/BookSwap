c6eb710b48a3cc88dd9987d627d1a809b38e77d2
Harsanjit Bhullar <harsanjitb1@gmail.com>
2023-11-20 10:36:36 -0700

    Adding Registration Page

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const RegisterPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle registration logic here
+        console.log('Register with:', email, password, confirmPassword);
+        // Redirect or show error messages based on registration success or failure
+    };
+
+    return (
+        <div className="register_page">
+            <h2>Register</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Confirm Password:</label>
+                    <input
+                        type="password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+            <p>
+                Already have an account? <Link to="/login">Login here</Link>
+            </p>
+        </div>
+    );
+}
+
+export default RegisterPage;
