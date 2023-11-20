b522e2e4cab59cdaac69ad13e8584aacbbce8b7a
Harsanjit <harsanjitb1@gmail.com>
2023-11-20 10:34:54 -0700

    Adding Register Page

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle login logic here
+        console.log('Login with:', email, password);
+        // Redirect or show error messages based on login success or failure
+    };
+
+    return (
+        <div className="login_page">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+            <p>
+                Don't have an account? <Link to="/register">Register here</Link>
+            </p>
+        </div>
+    );
+}
+
+export default LoginPage;
