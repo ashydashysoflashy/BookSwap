2083293f956d6193d46c9b8a72a0e4a2871997f9
Umar Ahmed <108314943+umar-ahmed1@users.noreply.github.com>
2023-11-22 18:19:49 -0700

    filters section done for now

--- frontend/src/components/Search/SearchFilters.jsx
+++ frontend/src/components/Search/SearchFilters.jsx
@@ -6,10 +6,29 @@ import { IoFilter } from "react-icons/io5";
 
 export default function SearchFilters() {
     const optionsLocation = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
+        { value: 'Calgary', label: 'Calgary' },
+        { value: 'Airdrie', label: 'Airdrie' },
+        { value: 'Chestermere', label: 'Chestermere' }
+    ]
+
+    //These will be retrieved from database later
+    const optionsCourseCode = [
+        { value: 'CPSC599', label: 'CPSC 599' },
+        { value: 'SENG513', label: 'SENG 513' },
+        { value: 'CPSC457', label: 'CPSC 457' }
+    ]
+
+    const optionsPrice = [
+        { value: '5', label: '$5' },
+        { value: '10', label: '$10' },
+        { value: '15', label: '$15' }
+    ]
+
+    const optionsSchool = [
+        { value: 'uofc', label: 'University of Calgary' },
+        { value: 'mru', label: 'Mount Royal University' },
+        { value: 'sait', label: 'SAIT' }
+    ]
 
   return (
     <div className='filters-container'>
@@ -19,9 +38,10 @@ export default function SearchFilters() {
             <IoFilter size={24} color='black'/>
         </div>
         <Select options={optionsLocation}   className="react-select-container" classNamePrefix="react-select" placeholder="Location" />
-        <Select options={optionsLocation}   className="react-select-container" classNamePrefix="react-select" placeholder="Course Code" />
-        <div>Price Dropdown</div>
-        <Select options={optionsLocation}   className="react-select-container" classNamePrefix="react-select" placeholder="School" />
+        <Select options={optionsCourseCode} className="react-select-container" classNamePrefix="react-select" placeholder="Course Code" />
+        <Select options={optionsPrice}   className="react-select-container" classNamePrefix="react-select" placeholder="Minimum Price" />
+        <Select options={optionsPrice}   className="react-select-container" classNamePrefix="react-select" placeholder="Maximum Price" />
+        <Select options={optionsSchool}   className="react-select-container" classNamePrefix="react-select" placeholder="School" />
     </div>
   )
 }
