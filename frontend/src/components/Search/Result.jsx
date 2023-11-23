7502e2b6bac7c4de47c26a2a5c0048536d6e5900
Umar Ahmed <108314943+umar-ahmed1@users.noreply.github.com>
2023-11-22 18:31:42 -0700

    started on search results

---
+++ frontend/src/components/Search/Result.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Result.css'
+
+export default function ResultItem() {
+  return (
+    <div className='result-container'>
+        ResultItem
+    </div>
+  )
+}
